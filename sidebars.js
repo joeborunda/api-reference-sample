b87438d4ad7acd2d7c164f1858e8a2bc8f753e6b
joeborunda <joseborunda@gmail.com>
2025-11-11 13:47:47 -0600

    Fix sidebars.js syntax

--- sidebars.js
+++ sidebars.js
@@ -1,14 +1,18 @@
 // @ts-check
 
-  
- - render a sidebar for each doc of that group
- - provide next/previous navigation
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
- The sidebars can be generated from the filesystem, or explicitly defined here.
-
- Create as many sidebars as you want.
-
- @type {import('@docusaurus/plugin-content-docs').SidebarsConfig}
+/**
+ * Creating a sidebar enables you to:
+ *  - create an ordered group of docs
+ *  - render a sidebar for each doc of that group
+ *  - provide next/previous navigation
+ *
+ * The sidebars can be generated from the filesystem, or explicitly defined here.
+ *
+ * Create as many sidebars as you want.
+ *
+ * @type {import('@docusaurus/plugin-content-docs').SidebarsConfig}
  */
 const sidebars = {
   tutorialSidebar: [
@@ -21,10 +25,6 @@ const sidebars = {
         'errors',
       ],
     },
-    allDocsSidebar: [
-      'sample1',
-      'sample2',
-    ],
     {
       type: 'category',
       label: 'API Reference',
